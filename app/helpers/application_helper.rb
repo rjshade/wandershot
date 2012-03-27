b7fe619fd9b0cc4829522856b0a9d2c1c5817070
rjshade <robbie.shade@gmail.com>
2012-03-27 11:11:07 +0100

    Logo helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def wandershot_text( darkbg = false )
+    "<span class='wandershot-logo #{darkbg ? "dark-bg" : "light-bg"}'><span class='wander'>wander</span><span class='shot'>shot</span></span>".html_safe
+  end
 end
