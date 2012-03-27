12046dae2e4f246c1045f664cd83559b3d82bc3c
rjshade <robbie.shade@gmail.com>
2012-03-27 16:30:21 +0100

    Homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
+    @post_main = Post.all.shuffle.first
+    @post_second = Post.all.shuffle.first
+    @post_third = Post.all.shuffle.first
   end
 end
