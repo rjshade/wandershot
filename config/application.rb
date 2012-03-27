379ce8cf6e601fc85a9e7c04779beee16145597c
rjshade <robbie.shade@gmail.com>
2012-03-27 16:23:01 +0100

    SimpleForm initializers

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Wandershot
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/lib/extras)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
