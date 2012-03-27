fc7f0313abbee94b9b6c5532fbc63cba688d1c4d
rjshade <robbie.shade@gmail.com>
2012-03-27 16:23:17 +0100

    Stock images for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,13 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Slug
   include Mongoid::Timestamps
 
   field :title, :type => String
+  slug :title
+
   field :text, :type => String
+  field :image_path, :type => String
 
   belongs_to :story
 end
