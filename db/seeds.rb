28a687de5402e640689f537c6f071639192a46be
rjshade <robbie.shade@gmail.com>
2012-03-27 23:22:18 +0100

    Story display

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,10 @@ summary = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam moles
 Integer varius aliquet vulputate. Fusce id purus felis, vitae commodo lacus. Suspendisse potenti. Vivamus fringilla consectetur ligula, posuere volutpat nisl lobortis eu. In mauris velit, mattis vitae accumsan quis, lacinia quis ipsum. Quisque lorem enim, cursus in dignissim id, mollis vitae elit. Maecenas arcu quam, semper eu vestibulum ut, elementum eu metus. Donec risus diam, hendrerit quis sagittis et, rutrum consectetur elit. Aliquam non justo sit amet urna euismod ultricies. Nunc rhoncus egestas posuere.'
 
 
-3.times do |s|
+(rand(10)+1).times do |s|
   story = user.stories.create! title: "Exciting story #{s}", summary: summary
 
-  4.times do |i|
+  (rand(10)+1).times do |i|
     post = story.posts.create! title: "Entry number #{i}", text: summary, image_path: "stock/stock_#{(0..8).to_a.shuffle.first}.jpg"
   end
   puts "Story #{s} created"
