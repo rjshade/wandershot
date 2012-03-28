7266fb123050ce46bfde7f11f7150b7e6ef7933e
rjshade <robbie.shade@gmail.com>
2012-03-28 21:39:43 +0100

    S3 options

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,14 @@ class Post
   include Mongoid::Timestamps
   include Mongoid::Paperclip
 
-  has_mongoid_attached_file :image,
+  has_mongoid_attached_file :image, {
                             :styles => {
                               :original => ['1920x1680>', :jpg],
                               :thumb    => ['100x100#',   :jpg],
                               :medium   => ['400x400>',   :jpg],
-                              :large    => ['800x800>',   :jpg]
+                              :large    => ['800x800>',   :jpg] },
+
+                            :path => "/public/:user_slug/stories/:story_slug/:post_slug/:attachment/:id/:style.:extension"
                             }.merge(PAPERCLIP_STORAGE_OPTIONS)
 
   field :title, :type => String
