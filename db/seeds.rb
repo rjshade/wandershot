3ff009bb5fc3fc5280760dcd388edef3ed1be328
rjshade <robbie.shade@gmail.com>
2012-03-28 21:04:53 +0100

    Seed data with paperclip images

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,17 @@ summary = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam moles
 Integer varius aliquet vulputate. Fusce id purus felis, vitae commodo lacus. Suspendisse potenti. Vivamus fringilla consectetur ligula, posuere volutpat nisl lobortis eu. In mauris velit, mattis vitae accumsan quis, lacinia quis ipsum. Quisque lorem enim, cursus in dignissim id, mollis vitae elit. Maecenas arcu quam, semper eu vestibulum ut, elementum eu metus. Donec risus diam, hendrerit quis sagittis et, rutrum consectetur elit. Aliquam non justo sit amet urna euismod ultricies. Nunc rhoncus egestas posuere.'
 
 
-(rand(10)+1).times do |s|
+(rand(8)+1).times do |s|
   start_date = rand(1000).days.ago
   story = user.stories.create! title: "Exciting story #{s}", summary: summary, created_at: start_date
 
   (rand(10)+1).times do |i|
-    post = story.posts.create! title: "Entry number #{i}", text: summary, image_path: "stock/stock_#{(0..8).to_a.shuffle.first}.jpg", created_at: start_date + i.days
+    post = story.posts.create! title: "Entry number #{i}", text: summary, created_at: start_date + i.days
+      
+    if rand(100) > 70
+      post.image = File.open("#{Rails.root}/app/assets/images/stock/stock_#{(0..8).to_a.shuffle.first}.jpg")
+      post.save
+    end
   end
   puts "Story #{s} created"
 end
